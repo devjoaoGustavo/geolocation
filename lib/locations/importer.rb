dd37f20ff4511b8d9fd7a2ba38aefaceb1064f94
João Gustavo de Paula <dev.joaogustavo@gmail.com>
2022-08-29 23:18:02 -0300

    create ip location importer

--- lib/locations/importer.rb
+++ lib/locations/importer.rb
@@ -1,24 +1,57 @@
 require 'csv'
+require 'benchmark'
 
 module Locations
   class Importer
+    PERSISTENCE_BATCH_SIZE = 200_000
+    private_constant :PERSISTENCE_BATCH_SIZE
+
     attr_reader :validator, :persistence
 
-    def initialize(validator: nil, persistence: nil)
+    ##
+    # @param validator [Class]
+    # @param persistence [Class]
+    def initialize(validator: nil, persistence:)
       @validator = validator || Validator
-      @persistence = persistence || IPLocation
+      @persistence = persistence
     end
 
-    def import_from_file(filename)
-      records = []
+    ##
+    # @param filename [String]
+    def import_from_csv_file(filename)
+      discarted = 0
+      records = {}
+      seen = {}
 
-      CSV.foreach(filename, headers: true) do |hash|
-        next unless validator.call hash
+      Rails.logger.info 'starting...'
+      time = Benchmark.realtime {
+        validation = Benchmark.realtime {
+          CSV.foreach(filename, headers: true) do |row|
+            ip_address = row['ip_address']
+            next discarted += 1 if seen[ip_address]
 
-        records << hash
+            seen[ip_address] = true
+            next discarted += 1 unless validator.valid?(row)
+
+            records[ip_address] = row.to_h
+          end
+        }
+        Rails.logger.info "#{validation} seconds to validate"
+        persistence_time = Benchmark.realtime {
+          persist(records.values)
+        }
+        Rails.logger.info "#{persistence_time} seconds to persist"
+      }
+
+      Rails.logger.info "Time Elapsed: #{time} seconds."
+      Rails.logger.info "Records accepted: #{records.count}."
+      Rails.logger.info "Records discarted: #{discarted}."
+    end
+
+    def persist(records)
+      records.each_slice(PERSISTENCE_BATCH_SIZE) do |batch|
+        persistence.insert_all batch, returning: false
       end
-
-      persistence.upsert_all records
     end
   end
 end
--- spec/lib/locations/importer_spec.rb
+++ spec/lib/locations/importer_spec.rb
@@ -1,12 +1,12 @@
 require 'locations/importer'
 
 RSpec.describe Locations::Importer do
-  describe '.import_from_file' do
+  describe '.import_from_csv_file' do
     let(:validator) { double }
     let(:persistence) { double }
 
     before do
-      allow(validator).to receive(:call).and_return(true)
+      allow(validator).to receive(:valid?).and_return(true)
       allow(persistence).to receive(:upsert_all).and_return(true)
     end
 
@@ -14,15 +14,15 @@ RSpec.describe Locations::Importer do
       let(:filename) { 'spec/lib/locations/fake_locations.csv' }
 
       it 'calls the data validator for each parsed record' do
-        expect(validator).to receive(:call).exactly(6).times
+        expect(validator).to receive(:valid?).exactly(5).times
 
-        described_class.new(validator: validator, persistence: persistence).import_from_file(filename)
+        described_class.new(validator: validator, persistence: persistence).import_from_csv_file(filename)
       end
 
       it 'calls the persistence layer with parsed' do
         expect(persistence).to receive(:upsert_all).once
 
-        described_class.new(validator: validator, persistence: persistence).import_from_file(filename)
+        described_class.new(validator: validator, persistence: persistence).import_from_csv_file(filename)
       end
     end
 
@@ -31,7 +31,7 @@ RSpec.describe Locations::Importer do
 
       it 'raises an error' do
         expect do
-          described_class.new(validator: validator, persistence: persistence).import_from_file(filename)
+          described_class.new(validator: validator, persistence: persistence).import_from_csv_file(filename)
         end.to raise_error(Errno::ENOENT)
       end
     end
